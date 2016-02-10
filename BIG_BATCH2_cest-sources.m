f2b959feb164e069ade4b2b57b57231e7d104f2d
mzaiss <altustro@gmail.com>
2016-02-10 17:13:58 +0100

    GPL

--- BIG_BATCH2_cest-sources.m
+++ BIG_BATCH2_cest-sources.m
@@ -1,3 +1,20 @@
+%% CEST EVAL
+%   Date: 2016/02/10 
+%   Version for CEST-sources.de
+%   Authors: Moritz Zaiss  - m.zaiss@dkfz.de , Johannes Windschuh 
+%   Divison of Medical Physics in Radiology
+%   German Cancer Research Center (DKFZ), Heidelberg Germany, http://www.dkfz.de/en/index.html
+%   CEST sources - Copyright (C) 2016  Moritz Zaiss
+%   **********************************
+%   This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or(at your option) any later version.
+%    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+%    You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%   **********************************
+%
+%   --SHORT  DOC--
+
 %% add current folder and subfolders to path
 addpath(genpath(cd('.')))
 
