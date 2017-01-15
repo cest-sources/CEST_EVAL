699c59c8fe416b274a19cd264fcbaaa8522842b3
catetrai <caterina.trainito@gmail.com>
2017-01-15 11:08:39 +0100

    Improved 'matlabfit' error handlingin try..catch statement

--- levmar_fit/matlabfit.m
+++ levmar_fit/matlabfit.m
@@ -6,13 +6,13 @@ function [ret, popt, info, covar] = matlabfit(fitfunc, p0, M, nIter, options,bou
 %to be split up in seperate variables, that is a bit stupid to programm to
 %be general working, i implement it for the optimization toolbox firt.
 %
-% Algorithm — Algorithm used for the fitting procedure:
-% Trust-Region — This is the default algorithm and must be used if you specify Lower or Upper coefficient constraints.
-% Levenberg-Marquardt — If the trust-region algorithm does not produce a reasonable fit, and you do not have coefficient constraints, try the Levenberg-Marquardt algorithm.
+% Algorithm ? Algorithm used for the fitting procedure:
+% Trust-Region ? This is the default algorithm and must be used if you specify Lower or Upper coefficient constraints.
+% Levenberg-Marquardt ? If the trust-region algorithm does not produce a reasonable fit, and you do not have coefficient constraints, try the Levenberg-Marquardt algorithm.
 % Finite Differencing Parameters
 % 
-% DiffMinChange — Minimum change in coefficients for finite difference Jacobians. The default value is 10-8.
-% DiffMaxChange — Maximum change in coefficients for finite difference Jacobians. The default value is 0.1.
+% DiffMinChange ? Minimum change in coefficients for finite difference Jacobians. The default value is 10-8.
+% DiffMaxChange ? Maximum change in coefficients for finite difference Jacobians. The default value is 0.1.
 % Note that DiffMinChange and DiffMaxChange apply to:
 % 
 % Any nonlinear custom equation, that is, a nonlinear equation that you write
@@ -21,17 +21,17 @@ function [ret, popt, info, covar] = matlabfit(fitfunc, p0, M, nIter, options,bou
 % 
 % Fit Convergence Criteria
 % 
-% MaxFunEvals — Maximum number of function (model) evaluations allowed. The default value is 600.
-% MaxIter — Maximum number of fit iterations allowed. The default value is 400.
-% TolFun — Termination tolerance used on stopping conditions involving the function (model) value. The default value is 10-6.
-% TolX — Termination tolerance used on stopping conditions involving the coefficients. The default value is 10-6.
+% MaxFunEvals ? Maximum number of function (model) evaluations allowed. The default value is 600.
+% MaxIter ? Maximum number of fit iterations allowed. The default value is 400.
+% TolFun ? Termination tolerance used on stopping conditions involving the function (model) value. The default value is 10-6.
+% TolX ? Termination tolerance used on stopping conditions involving the coefficients. The default value is 10-6.
 % Coefficient Parameters
 % 
-% Unknowns — Symbols for the unknown coefficients to be fitted.
-% StartPoint — The coefficient starting values. The default values depend on the model. For rational, Weibull, and custom models, default values are randomly selected within the range [0,1]. For all other nonlinear library models, the starting values depend on the data set and are calculated heuristically. See optimized starting points below.
-% Lower — Lower bounds on the fitted coefficients. The tool only uses the bounds with the trust region fitting algorithm. The default lower bounds for most library models are -Inf, which indicates that the coefficients are unconstrained. However, a few models have finite default lower bounds. For example, Gaussians have the width parameter constrained so that it cannot be less than 0. See default constraints below.
-% Upper — Upper bounds on the fitted coefficients. The tool only uses the bounds with the trust region fitting algorithm. The default upper bounds for all library models are Inf, which indicates that the coefficients are unconstrained.
-% For more information about these fit options, see the lsqcurvefit function in the Optimization Toolbox™ documentation.
+% Unknowns ? Symbols for the unknown coefficients to be fitted.
+% StartPoint ? The coefficient starting values. The default values depend on the model. For rational, Weibull, and custom models, default values are randomly selected within the range [0,1]. For all other nonlinear library models, the starting values depend on the data set and are calculated heuristically. See optimized starting points below.
+% Lower ? Lower bounds on the fitted coefficients. The tool only uses the bounds with the trust region fitting algorithm. The default lower bounds for most library models are -Inf, which indicates that the coefficients are unconstrained. However, a few models have finite default lower bounds. For example, Gaussians have the width parameter constrained so that it cannot be less than 0. See default constraints below.
+% Upper ? Upper bounds on the fitted coefficients. The tool only uses the bounds with the trust region fitting algorithm. The default upper bounds for all library models are Inf, which indicates that the coefficients are unconstrained.
+% For more information about these fit options, see the lsqcurvefit function in the Optimization Toolbox? documentation.
 
 
 %  fitable_func =@(x,w,Z) feval(fitfunc,x, w, P);
@@ -80,12 +80,11 @@ matlab_options = optimset('TolFun',options(4),'TolX',options(3), 'MaxIter',nIter
     popt=0;
     
 try
-[popt resnorm RES,EXITFLAG,OUTPUT,LAMBDA,JACOBIAN] = lsqcurvefit(fitable_func,p0,w,M,lb,ub,matlab_options);
-ret=EXITFLAG;
-info=OUTPUT;
-catch
-    warning('lsqrfit retrned with an error');
- end;
+    [popt resnorm RES,EXITFLAG,OUTPUT,LAMBDA,JACOBIAN] = lsqcurvefit(fitable_func,p0,w,M,lb,ub,matlab_options);
+catch ME
+    warning('lsqrfit returned with an error');
+    ME
+end;
 
 try
 [ci, varb, corrb, varinf] = nlparci(popt,RES,JACOBIAN,0.95);
