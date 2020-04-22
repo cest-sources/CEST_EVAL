ccd0f42492c16e39ea5c03ca17afca73bb437344
mzaiss <moritz.zaiss@uk-erlangen.de>
2020-04-22 12:54:33 +0200

    added BW Lorentz fits

---
+++ levmar_fit/lorentzfit2pool_BW_rel.m
@@ -0,0 +1,7 @@
+function x = lorentzfit2pool_BW_rel(p, w, P)
+
+%will Gamma in Hz;  db0 in ppm:  p(4) und p(7) sind in ppm jetzt
+BW=p(8);
+
+x= p(1)-p(2).*p(3).^2./4./(p(3).^2/4+((w-p(4)-BW/2).*heaviside(w-BW/2-p(4))+(w-p(4)+BW/2).*heaviside(-(w+BW/2-p(4)))).^2) - p(5).*p(6).^2./4./(p(6).^2./4+(w-p(4)).^2);
+    
---
+++ levmar_fit/lorentzfit6pool_BW.m
@@ -0,0 +1,15 @@
+function x = lorentzfit6pool_BW(p, w, P)
+% LORENTZFIT6POOl_BW
+% p parameter vector p(1) baseline,
+% p(2) amplitude of Lorentz, p(3) width of Lorentz, p(4) position of Lorentz
+% p(5) amplitude of Lorentz, p(6) width of Lorentz, p(7) position of Lorentz
+% and so on
+% p(20) bandwith of the plateau within the water loretz given by [p(2) p(3), p(4)]
+
+% w is the offset axis vector
+% P is a vector of Parameters
+
+BW=p(20);
+x= p(1)-p(2).*p(3).^2./4./(p(3).^2/4+((w-p(4)-BW/2).*heaviside(w-BW/2-p(4))+(w-p(4)+BW/2).*heaviside(-(w+BW/2-p(4)))).^2)...
+- p(5).*p(6).^2./4./(p(6).^2./4+(w-p(7)).^2) - p(8).*p(9).^2./4./(p(9).^2./4.+(w-p(10)).^2)- p(11).*p(12).^2./4./(p(12).^2./4.+(w-p(13)).^2)- p(14).*p(15).^2./4./(p(15).^2/4.+(w-p(16)).^2)- p(17).*p(18).^2./4./(p(18).^2/4.+(w-p(19)).^2);
+  
